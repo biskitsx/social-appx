d562a7b44b6d0aae30feb53b0c59cd9f7fdec429
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-06-23 12:26:39 +0700

    fix .env

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -1,54 +1,48 @@
 import cloudinary from 'cloudinary'
 
-// cloudinary.config({ 
-//     cloud_name: process.env.CLOUD_NAME, 
-//     api_key: process.env.CLOUD_API_KEY, 
-//     api_secret: process.env.CLOUD_API_SECRET 
-// });
-
-cloudinary.config({ 
-    cloud_name: 'dpbro2ayv', 
-    api_key: '698997354456696', 
-    api_secret: 'ei9Ir1UF4mKpVvohcAe6NAj36gY' 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
 });
 
 
 export const cloudinaryUploadImg = async (file) => {
     try {
-        return new Promise ((resolve) => {
+        return new Promise((resolve) => {
             cloudinary.uploader.upload(file, (result) => {
                 resolve({
                     url: result.secure_url,
                     asset_id: result.asset_id,
                     public_id: result.public_id
                 },
-                {
-                    resource_type: "auto"
-                })
+                    {
+                        resource_type: "auto"
+                    })
             })
         })
-        
-    } catch(error) {
+
+    } catch (error) {
         console.error(error)
     }
 }
 
 export const cloudinaryDeleteImg = async (file) => {
     try {
-        return new Promise ((resolve) => {
+        return new Promise((resolve) => {
             cloudinary.uploader.destroy(file, (result) => {
                 resolve({
                     url: result.secure_url,
                     asset_id: result.asset_id,
                     public_id: result.public_id
                 },
-                {
-                    resource_type: "auto"
-                })
+                    {
+                        resource_type: "auto"
+                    })
             })
         })
-        
-    } catch(error) {
+
+    } catch (error) {
         console.error(error)
 
     }
